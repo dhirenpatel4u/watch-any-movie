d65dbc07ec290a10a2c78c0ad65cc060b78538ce
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-07-20 10:56:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
-import "./App.css";
 
 export default function App() {
+
+    const [search, setSearch] = useState("");
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/watch/:id" element={<Watch />} />
-        </Routes>
+        <>
+            <Navbar
+                search={search}
+                setSearch={setSearch}
+            />
+
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home
+                            search={search}
+                        />
+                    }
+                />
+
+                <Route
+                    path="/watch/:id"
+                    element={
+                        <Watch />
+                    }
+                />
+            </Routes>
+        </>
     );
 }
