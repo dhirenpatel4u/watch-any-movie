0740b05c6c2120b581c8c24f4ec85bd893108a1c
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-07-20 10:56:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,44 @@
+import { Link, useLocation } from "react-router-dom";
+
 export default function Navbar({
     search,
     setSearch
 }) {
+
+    const location = useLocation();
+
+    const isHome =
+        location.pathname === "/";
+
+    const isWatch =
+        location.pathname.startsWith("/watch/");
+
     return (
         <nav className="navbar">
 
-            <h1>Movie OTT</h1>
+            <Link to="/">
+                <h1>Watch Any Movie</h1>
+            </Link>
 
-            <input
-                type="text"
-                placeholder="Search movies..."
-                value={search}
-                onChange={(e) =>
-                    setSearch(e.target.value)
-                }
-            />
+            {isHome && (
+                <input
+                    type="text"
+                    placeholder="Search movies..."
+                    value={search}
+                    onChange={(e) =>
+                        setSearch(e.target.value)
+                    }
+                />
+            )}
+
+            {isWatch && (
+                <Link
+                    to="/"
+                    className="home-btn"
+                >
+                    Home
+                </Link>
+            )}
 
         </nav>
     );
