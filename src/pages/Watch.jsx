382d90a4a7a5328d9bce8cdd0d00ff355a6cedc3
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-07-20 11:45:17 +0530

    Update Watch.jsx

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -8,15 +8,53 @@ export default function Watch() {
 
     const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
+useEffect(() => {
 
-        fetch(
-            "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRVaQVJXRUjmOv7OVWraFMF4rvcJlXXamDr4hIAz54OqSYcXP_Zq6vyW-a_TDVHSe_qe_o51RZiV46RGIrHzqIxjbRUqA7ABWdSGdRVX2Pl9yFlQtGLuAzB27VFpiORg2D4iLOtd-eoruV0HrqUuomTCk_cfdmgOdzFkCBFqRiKKAZ2eZxy4Rz4qsjQvJ4Wk2E3w1c8DdpQWtRFFho0dHqM3LydDH4U2m696GKnlZlDJvHPQ8Ieg77izbeNNthNp4ArtnWgjihn0HkfoCG0-bxYWADwvQ&lib=MdkOOLVUqxpsMtamGk_CUO1T3b6iDXk-u"
-        )
-            .then((r) => r.json())
-            .then((data) => setMovies(data.data));
+    const CACHE_KEY = "movies";
+    const CACHE_TIME = "movies_time";
 
-    }, []);
+    const movies =
+        localStorage.getItem(CACHE_KEY);
+
+    const time =
+        localStorage.getItem(CACHE_TIME);
+
+    const now = Date.now();
+
+    if (
+        movies &&
+        time &&
+        now - Number(time)
+            < 24 * 60 * 60 * 1000
+    ) {
+
+        setMovies(
+            JSON.parse(movies)
+        );
+
+        return;
+    }
+
+    fetch(
+        "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRVaQVJXRUjmOv7OVWraFMF4rvcJlXXamDr4hIAz54OqSYcXP_Zq6vyW-a_TDVHSe_qe_o51RZiV46RGIrHzqIxjbRUqA7ABWdSGdRVX2Pl9yFlQtGLuAzB27VFpiORg2D4iLOtd-eoruV0HrqUuomTCk_cfdmgOdzFkCBFqRiKKAZ2eZxy4Rz4qsjQvJ4Wk2E3w1c8DdpQWtRFFho0dHqM3LydDH4U2m696GKnlZlDJvHPQ8Ieg77izbeNNthNp4ArtnWgjihn0HkfoCG0-bxYWADwvQ&lib=MdkOOLVUqxpsMtamGk_CUO1T3b6iDXk-u"
+    )
+        .then((r) => r.json())
+        .then((data) => {
+
+            setMovies(data.data);
+
+            localStorage.setItem(
+                CACHE_KEY,
+                JSON.stringify(data.data)
+            );
+
+            localStorage.setItem(
+                CACHE_TIME,
+                now
+            );
+        });
+
+}, []);
 
     const random = [...movies]
         .sort(() => Math.random() - 0.5)
