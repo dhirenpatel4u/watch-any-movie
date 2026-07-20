919bc67a5471d5c7756f06cbc95462174458f265
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-07-20 10:35:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
+import "./App.css";
 
 export default function App() {
     return (
