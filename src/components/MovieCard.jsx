acf4fc2c4222778f4202b7f5afa33a913aa68b30
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-08-15 15:04:01 +0530

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }) {
     const poster =
-        `https://m.media-amazon.com/images/M/${movie.Poster}`;
+        `${movie.Poster}`;
 
     return (
         <Link
