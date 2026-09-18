2965484ff415ef023acc5afc1bcecc5fead00ea6
dhirenpatel4u <102461109+dhirenpatel4u@users.noreply.github.com>
2026-09-18 14:30:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-plugins: [react()],
-
-build: {
-    rollupOptions: {
-        output: {
-            // Keep predictable filenames so the
-            // server-generated watch page can load
-            // the React application correctly.
-
-            entryFileNames: "assets/app.js",
-
-            chunkFileNames: "assets/[name].js",
-
-            assetFileNames: "assets/[name][extname]"
-        }
-    }
-}
-    
+plugins: [react()]
 });
